d5f17cd8c1a6c673d80ac80d9c452cd07246a1c6
triciasykes <tlsykes98@gmail.com>
2022-10-25 14:08:48 -0700

    adds model and request specs/validations

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -7,7 +7,11 @@ class CatsController < ApplicationController
 
   def create
     cat = Cat.create(cat_params)
-    render json: cat
+    if cat.valid?
+      render json: cat
+    else
+      render json: cat.errors, status: 422
+    end
   end  
   
   def update
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,4 @@
 class Cat < ApplicationRecord
+    validates :name, :age, :enjoys, :image, presence: true
+    validates :enjoys, length: { minimum: 10 }
 end
